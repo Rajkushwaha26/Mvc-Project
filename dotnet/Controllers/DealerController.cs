d010df49b5bf9996d3ec7e3c21b90f888dd7095b
Iamneo <techadmin@iamneo.ai>
2023-11-01 16:21:42 +0000

    Backup Commit

---
+++ dotnet/Controllers/DealerController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dotnet.Controllers
+{
+    
+    public class DealerController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public IActionResult Index()
+        {
+            var item = db.Dealer;
+
+            return View(item);
+        }
+        public IActionResult Create(){
+            return View();
+        }
+        public IActionResult Delete(){
+            return View();
+        }
+        public IActionResult Edit(){
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
