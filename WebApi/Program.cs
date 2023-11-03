09f1c05ab4a193c8a65e1e1cca6de2d1a90242dd
Iamneo <techadmin@iamneo.ai>
2023-11-03 11:25:44 +0000

    Backup Commit

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(Options=>{
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("DbConn"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
