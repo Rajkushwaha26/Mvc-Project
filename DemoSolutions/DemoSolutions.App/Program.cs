7bbf071c1ad1f24332b6fedd65a7ec049107407c
Iamneo <techadmin@iamneo.ai>
2023-11-08 06:12:03 +0000

    Backup Commit

---
+++ DemoSolutions/DemoSolutions.App/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace DemoSolutions{
+    public class Program{
+        public static void Main()
+        {
+
+        Console.WriteLine("Hell Rohit, this Raj");
+        }
+    public int Demo(){
+        return 100;
+    }
+    }
+}
+
