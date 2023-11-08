0ef49be79a313a177e5afb668d4fcfca6f8dcf91
Iamneo <techadmin@iamneo.ai>
2023-11-08 09:17:29 +0000

    Backup Commit

--- DemoSolutions/DemoSolutions.Test/UnitTest1.cs
+++ DemoSolutions/DemoSolutions.Test/UnitTest1.cs
@@ -1,9 +1,39 @@
 //using DemoSolutions;
+using DemoSolutions.WebApi.Controllers;
 namespace DemoSolutions.Test;
 
+ class Dispossible : IDisposable
+    {
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+    public class MockObject : ILogger<WeatherForecastController>
+    {
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return new Dispossible();
+            //throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return false;
+            //throw new NotImplementedException();
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+           // throw new NotImplementedException();
+        }
+    }
 public class Tests
 {
+    
     Program program= null;
+
+    WeatherForcasteController controllerforecast = null;
     [SetUp]
     public void Setup()
     {
