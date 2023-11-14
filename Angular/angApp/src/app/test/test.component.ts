90de7d6252605d26c04faaf8e0a11b56ee9a9082
Iamneo <techadmin@iamneo.ai>
2023-11-14 07:33:45 +0000

    Backup Commit

--- Angular/angApp/src/app/test/test.component.ts
+++ Angular/angApp/src/app/test/test.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template:`
+  <h2>
+  Welcome{{name}}`
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
