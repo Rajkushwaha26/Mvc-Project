05e61d5ee9a55143c10f5397918f60b2ee7553cf
Iamneo <techadmin@iamneo.ai>
2023-11-14 13:55:07 +0000

    Backup Commit

--- Angular/angApp/src/app/test/test.component.ts
+++ Angular/angApp/src/app/test/test.component.ts
@@ -4,14 +4,18 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   template:`
   <h1>
-    Welcome To {{name}}
+    I Am From {{name}}
   </h1>
   <h3>{{2+2}}</h3>
+  <h2>{{name}}<h2>
+  <h3>{{name | lowercase}}</h3>
+  <h3>{{name | uppercase}}</h3>
+  <h2>{{mesaage | titlecase}}</h2>
+  <h2>{{name | slice:3:7}}</h2>
   
- 
+ <h1>{{date | date:'shortDate'}}</
   
-  <input #myInput type = "text">
-  <button (click)="logMessage(myInput.value)">Submit</button>
+  
 
   
   
@@ -19,12 +23,14 @@ import { Component, OnInit } from '@angular/core';
   `,
    // <input [id]="myId" type="text" value="Vishwas">
   // <input [disabled]="isDisabled" id="{{myId}}" type ="text" value="Vishwas">
+  // <input #myInput type = "text">
+  // <button (click)="logMessage(myInput.value)">Submit</button>
   styles:[]
 })
 export class TestComponent implements OnInit {
 
-  public name="India";
-
+  public name="WelComeToIndia";
+  public message ="Welcome to Codeworld";
   public myId = "testId";
   public isDisabled=false;
 
