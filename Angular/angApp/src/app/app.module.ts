ec41fa73ea077183dfdb3ef5d31197a5c22d8f56
Iamneo <techadmin@iamneo.ai>
2023-11-15 06:58:04 +0000

    Backup Commit

--- Angular/angApp/src/app/app.module.ts
+++ Angular/angApp/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { EmployeeListComponent } from './Employee/employee-list.component';
+import {  DemoRoot } from './demo/demo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    DemoRoot
   ],
   imports: [
     BrowserModule,
---
+++ Angular/angApp/src/app/demo/demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'DemoRoot',
+  template: `<h1>Hello to Salonga</h1>`,
+  styleUrls: ['./demo.component.css']
+})
+export class DemoRoot implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
