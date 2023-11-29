81d59e645f56d2b97579948eb8d83c1d18e0f853
Iamneo <techadmin@iamneo.ai>
2023-11-29 04:14:34 +0000

    Backup Commit

--- CapstoneApi/login/Models/AppliactionDbContext.cs
+++ CapstoneApi/login/Models/AppliactionDbContext.cs
@@ -5,9 +5,8 @@ using System.Threading.Tasks;
 
 namespace login.Models
 {
-    public class User
+    public class AppliactionDbContext
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        
     }
 }
--- CapstoneApi/login/Models/Login.cs
+++ CapstoneApi/login/Models/Login.cs
@@ -5,14 +5,9 @@ using System.Threading.Tasks;
 
 namespace login.Models
 {
-    public class Register
+    public class Login
     {
-         public int UserId { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public string Gender {get;set;}
-        public string Email {get;set;}
-
-        public string Role {get;set;}
     }
 }
