5977bd90646bf3a7b9f7ee2c4ab1bbd2be0c1f70
Iamneo <techadmin@iamneo.ai>
2023-12-08 11:37:02 +0000

    Auto-save-commit-08/12/2023--17:07:03

--- FoodorderingApi/dotnetapp/Controllers/OrderController.cs
+++ FoodorderingApi/dotnetapp/Controllers/OrderController.cs
@@ -16,6 +16,49 @@ namespace dotnetapp.Controllers
         public OrderController(ApplicationDbContext context){
             this.db=context;
         }
+//Menu
+        [HttpGet]
+        [Route("GetMenus")]
+        public IActionResult GetMenus(){
+            var menulist=db.Menus;
+            return Ok(menulist);
+        }
+
+        [HttpPost]
+        [Route("PostMenus")]
+        public IActionResult PostMenus(Menu b){
+            db.Menus.Add(b);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("PutMenus")]
+        public IActionResult PutMenus(int id, Menu menu ){
+            var menulist=db.Menus.Find(id);
+            if(menulist!=null){
+                menulist.Name=menu.Name;
+                menulist.Description=menu.Description;
+                menulist.Price=menu.Price;
+                db.Update(menulist);
+                db.SaveChanges();
+                return Ok();
+            }
+            else{
+                return NotFount("In Valid Input");
+            }
+        }
+
+        [HttpDelete]
+        [Route("DeleteMenu")]
+        public IActionResult DeleteMenu(int id){
+            var menulist
+        }
+
+
+
+
+
 
         [HttpGet]
         [Route("GetOrderDetail")]
